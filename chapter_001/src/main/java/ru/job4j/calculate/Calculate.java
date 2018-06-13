5bf9eae8b99d4de59799722b481ca864304b38d7
RafikovOleg <olegr282@gmail.com>
2018-06-13 23:59:53 +0500

    1.4. JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -2,15 +2,23 @@ package ru.job4j;
 
 /**
  * Class Calculate решение задачи части 001 урок 1.
- * @author rafikov
+ * @author Oleg Rafikov
  * @since 29.05.2018
  */
 public class Calculate {
 	/**
 	* Конструктор, вывод строки в консоль.
-	* @param arg -arg.
+	* @param args-args.
 	*/
     public static void main(String[] args) {
         System.out.println("Hello World");
+    }
+	 /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
     }
 }
